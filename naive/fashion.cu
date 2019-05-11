e40f0bd906f19ef32a35dc11af4e5402b41cde2a
Ren Liu <renliu@ghc27.ghc.andrew.cmu.edu>
2019-05-11 12:57:34 -0400

    Simple CUDA test

---
+++ naive/fashion.cu
@@ -0,0 +1,11 @@
+#include "fashion.h"
+
+const char* PATH_TRAIN_DATA = "../data/train-images-idx3-ubyte";
+const char* PATH_TRAIN_LABEL = "../data/train-labels-idx1-ubyte";
+const char* PATH_TEST_DATA = "../data/t10k-images-idx3-ubyte";
+const char* PATH_TEST_LABEL = "../data/t10k-labels-idx1-ubyte";
+
+int main() {
+    printf("CUDA is working");
+    return 0;
+}
---
+++ naive/fashion.h
@@ -0,0 +1,9 @@
+#ifndef FASHION_H
+#define FASHION_H
+
+// C
+#include <stdio.h>
+// CUDA
+#include <thrust/host_vector.h>
+
+#endif
