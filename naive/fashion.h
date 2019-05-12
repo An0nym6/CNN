c88101a7981f057294cb7219a3c1729602818c08
An0nym6 <liuren6@gmail.com>
2019-05-11 22:45:57 -0400

    Modify the train and test function

--- naive/fashion.h
+++ naive/fashion.h
@@ -285,6 +285,7 @@ public:
 
 #define gpuErrchk(ans)                                                         \
   { gpuAssert((ans), __FILE__, __LINE__); }
+
 inline void gpuAssert(cudaError_t code, const char *file, int line,
                       bool abort = true) {
   if (code != cudaSuccess) {
--- naive/gpu_net.cu
+++ naive/gpu_net.cu
@@ -22,99 +22,87 @@ GPU_Net::GPU_Net() {
 // Desctuctor
 GPU_Net::~GPU_Net() {}
 
-void GPU_Net::train(host_vector<host_vector<float>> &Xtrain,
-                    host_vector<int> &Ytrain) {
-  dim3 threadsPerBlock(TILE_WIDTH, TILE_WIDTH);
-  cout << "GPU_Net Train Start" << endl << fflush;
+// Train on training set
+void GPU_Net::train(host_vector<host_vector<float>> &x_train,
+                    host_vector<int> &y_train) {
+  cout << "Train Start" << endl << fflush;
+  // Output variables
   int minibatch_index;
-  double elapsed;
   struct timespec start, finish;
+  double elapsed;
   int fine_epoch = 0;
-  float pre_acc = 0;
   float max_acc = 0;
-  for (int epoch = 1; epoch <= 100; epoch++) {
+  // Run 100 epoches
+  for (int epoch = 1; epoch <= 10; epoch++) {
     minibatch_index = 0;
     correct_num = 0;
     elapsed = 0;
+    // Run NUM_TRAIN / MINIBATCH batches
     while (minibatch_index < NUM_TRAIN / MINIBATCH) {
       clock_gettime(CLOCK_REALTIME, &start);
-      conv1.X = Xtrain[minibatch_index];
-
+      // Convolution layer forward propagation
+      conv1.X = x_train[minibatch_index];
       conv1.forward_GPU_naive();
-
+      // Pooling layer forward propagation
       pool1.forward_GPU_naive(conv1.Output);
       forward_bias_per_channel(
           pool1.Output, pool1.b, MINIBATCH, pool1.Outputimage_channel,
           pool1.Outputimage_height, pool1.Outputimage_width);
-
       forward_relu(pool1.Output, fc1.X);
-
+      // Fully connected layer 1 forward propagation
       fc1.forward();
-
       forward_relu(fc1.Output, fc2.X);
-
+      // Fully connected layer 2 forward propagation
       fc2.forward();
-
       forward_relu(fc2.Output, fc3.X);
-
+      // Fully connected layer 3 forward propagation
       fc3.forward();
-
       forward_relu(fc3.Output, fc4.X);
-
+      // Fully connected layer 4 forward propagation
       fc4.X = fc3.Output;
       fc4.forward();
       fc4.Output_c = fc4.Output;
-
-      //// SoftMax Input = 10 Output = 10
-      sm1.accuracy(MINIBATCH, 10, Xtrain, Ytrain, fc4.Output_c, minibatch_index,
-                   correct_num);
-
-      sm1.softmax(MINIBATCH, 10, Ytrain, fc4.Output_c);
-
-      sm1.cross_entropy_loss(MINIBATCH, Ytrain, fc4.Output_c, 10, sm1.loss,
+      // SoftMax forward propagation
+      sm1.accuracy(MINIBATCH, 10, x_train, y_train, fc4.Output_c,
+                   minibatch_index, correct_num);
+      sm1.softmax(MINIBATCH, 10, y_train, fc4.Output_c);
+      sm1.cross_entropy_loss(MINIBATCH, y_train, fc4.Output_c, 10, sm1.loss,
                              minibatch_index);
-
-      //// SoftMax delta
-      sm1.softmax_backward(MINIBATCH, Ytrain, fc4.Output_c, sm1.delta_c, 10,
+      // Softmax backward propagation
+      sm1.softmax_backward(MINIBATCH, y_train, fc4.Output_c, sm1.delta_c, 10,
                            minibatch_index);
-
-      //// fc backward
+      // Fully connected layer 4 backward propagation
       fc4.Output = sm1.delta_c;
       fc4.backward();
-
       backward_relu(fc3.Output, fc4.X);
-
+      // Fully connected layer 3 backward propagation
       fc3.Output = fc4.X;
       fc3.backward();
-
       backward_relu(fc2.Output, fc3.X);
-
+      // Fully connected layer 2 backward propagation
       fc2.Output = fc3.X;
       fc2.backward();
-
       backward_relu(fc1.Output, fc2.X);
-
+      // Fully connected layer 1 backward propagation
       fc1.backward();
-
       backward_relu(pool1.Output, fc1.X);
-
+      // Pooling layer backward propagation
       backward_bias_per_channel(
           pool1.Output, pool1.b, MINIBATCH, pool1.Output_height,
           pool1.Output_width, pool1.Outputimage_channel,
           pool1.Outputimage_height * pool1.Outputimage_width);
-
       pool1.backward_GPU(fc1.X);
-
+      // Convolution layer backward propagation
       conv1.Output = pool1.X;
       conv1.backward_GPU_gemm();
 
+      // Calculate output variables
       clock_gettime(CLOCK_REALTIME, &finish);
-
       elapsed += ((double)finish.tv_sec - start.tv_sec) +
                  ((double)finish.tv_nsec - start.tv_nsec) / 1000000000.0;
       minibatch_index += 1;
-      if ((minibatch_index * MINIBATCH) == 60000) {
-        float acc = (float)correct_num / (60000);
+      if ((minibatch_index * MINIBATCH) == NUM_TRAIN) {
+        float acc = (float)correct_num / NUM_TRAIN;
         if (acc > max_acc) {
           max_acc = acc;
           fine_epoch = epoch;
@@ -130,42 +118,46 @@ void GPU_Net::train(host_vector<host_vector<float>> &Xtrain,
   }
 }
 
+// Test on test set
 void GPU_Net::test(host_vector<host_vector<float>> &Xtest,
                    host_vector<int> &Ytest) {
-  dim3 threadsPerBlock(TILE_WIDTH, TILE_WIDTH);
-  cout << "GPU_Net Test Start" << endl << fflush;
+  cout << "Test Start" << endl << fflush;
+  // Output variables
   int minibatch_index = 0;
   struct timespec start, finish;
   double elapsed = 0;
   correct_num = 0;
   int fine_epoch = 0;
-  float pre_acc = 0;
-  float max_acc = 0;
+  // Test on NUM_TEST / MINIBATCH batches
   while (minibatch_index < NUM_TEST / MINIBATCH) {
     clock_gettime(CLOCK_REALTIME, &start);
+    // Convolution layer forward propagation
     conv1.X = Xtest[minibatch_index];
-
     conv1.forward_GPU_naive();
-
+    // Pooling layer forward propagation
     pool1.forward_GPU_naive(conv1.Output);
     forward_bias_per_channel(pool1.Output, pool1.b, MINIBATCH,
                              pool1.Outputimage_channel,
                              pool1.Outputimage_height, pool1.Outputimage_width);
     forward_relu(pool1.Output, fc1.X);
+    // Fully connected layer 1 forward propagation
     fc1.forward();
     forward_relu(fc1.Output, fc2.X);
+    // Fully connected layer 2 forward propagation
     fc2.forward();
     forward_relu(fc2.Output, fc3.X);
-    fc3.X = fc2.Output;
+    // Fully connected layer 3 forward propagation
     fc3.forward();
-    fc3.Output_c = fc3.Output;
-    sm1.accuracy(MINIBATCH, 10, Xtest, Ytest, fc3.Output_c, minibatch_index,
+    forward_relu(fc3.Output, fc4.X);
+    // Fully connected layer 4 forward propagation
+    fc4.X = fc3.Output;
+    fc4.forward();
+    fc4.Output_c = fc4.Output;
+    // SoftMax forward propagation
+    sm1.accuracy(MINIBATCH, 10, Xtest, Ytest, fc4.Output_c, minibatch_index,
                  correct_num);
-    sm1.softmax(MINIBATCH, 10, Ytest, fc3.Output_c);
-    sm1.cross_entropy_loss(MINIBATCH, Ytest, fc3.Output_c, 10, sm1.loss,
-                           minibatch_index);
-    sm1.softmax_backward(MINIBATCH, Ytest, fc3.Output_c, sm1.delta_c, 10,
-                         minibatch_index);
+    
+    // Calculate output variables
     clock_gettime(CLOCK_REALTIME, &finish);
     elapsed += ((double)finish.tv_sec - start.tv_sec) +
                ((double)finish.tv_nsec - start.tv_nsec) / 1000000000.0;
